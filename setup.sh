e220a155ecdaecfcc91b1f2b63ed0b1c06ebdf33
sbauer <stephan.bauer@immobilienscout24.de>
2016-02-23 08:37:34 +0100

    introduce subfolders to seperate concerns

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ brew install coreutils
 brew install zsh
 brew install tmux
 
-ln -sf $(grealpath ./profile) ~/.profile
-ln -sf $(grealpath ./tmux.conf) ~/.tmux.conf
-ln -sf $(grealpath ./zshrc.local) ~/.zshrc.local
-ln -sf $(grealpath ./vimrc) ~/.vimrc
-ln -sf $(grealpath ./zshrc) ~/.zshrc
+ln -sf $(grealpath ./shell/profile) ~/.profile
+ln -sf $(grealpath ./shell/tmux.conf) ~/.tmux.conf
+ln -sf $(grealpath ./shell/zshrc.local) ~/.zshrc.local
+ln -sf $(grealpath ./shell/vimrc) ~/.vimrc
+ln -sf $(grealpath ./shell/zshrc) ~/.zshrc
 
