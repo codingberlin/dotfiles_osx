1a8445854dd06a9727e6716d210362e27d666146
sbauer <stephan.bauer@immobilienscout24.de>
2017-05-03 09:39:07 +0200

    add unix support

--- setup.sh
+++ setup.sh
@@ -1,44 +1,57 @@
 #!/usr/bin/env bash
 set -u -e -E -C -o pipefail
 
-#brew
-which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew update
+USE_BREW=true
+brew update >/dev/null 2>&1 || USE_BREW=false 
 
-brew install wget
-brew install coreutils
-
-### zsh
-brew install zsh
-ln -sf $(grealpath ./shell/profile) ~/.profile
-ln -sf $(grealpath ./shell/zshrc) ~/.zshrc
-ln -sf $(grealpath ./shell/zshrc.local) ~/.zshrc.local
-ln -sf $(grealpath ./shell/ideavimrc) ~/.ideavimrc
-# https://stackoverflow.com/questions/13762280/zsh-compinit-insecure-directories/22753363
-sudo chmod -R 755 /usr/local/share/zsh
-sudo chown -R root:staff /usr/local/share/zsh
-
-brew install tmux
-ln -sf $(grealpath ./shell/tmux.conf) ~/.tmux.conf
-brew install tree
-brew install aspell
-brew install hunspell
-brew install sbt
-brew install ag
-brew install tig
-
-ln -sf $(grealpath ./shell/vimrc) ~/.vimrc
-ln -sf $(grealpath ./shell/gitconfig) ~/.gitconfig
-
-### git radar
-git submodule update --init --recursive
-if ! [ -d ~/bin ]; then
-  mkdir ~/bin
+if [ "$USE_BREW" = true ]; then
+  which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew update
+  brew install coreutils
+  INSTALL_COMMAND="brew install"
 fi
-ln -sf $(grealpath ./git-radar/git-radar) ~/bin/git-radar
 
-### vim
+if [ "$USE_BREW" = false ]; then
+  INSTALL_COMMAND="sudo pacman -S --needed --noconfirm"
+fi
+
+$INSTALL_COMMAND vim
+$INSTALL_COMMAND wget
+$INSTALL_COMMAND zsh
+$INSTALL_COMMAND tmux
+$INSTALL_COMMAND tree
+$INSTALL_COMMAND aspell
+$INSTALL_COMMAND hunspell
+$INSTALL_COMMAND sbt
+$INSTALL_COMMAND tig
+$INSTALL_COMMAND cargo
+
+git submodule update --init --recursive
 if ! [ -f ~/.vim/autoload/plug.vim ]; then
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
+[[ -f ~/.cargo/bin/dotcopter ]] || cargo install dotcopter
+~/.cargo/bin/dotcopter dotcopter.yaml apply
+
+if [ "$USE_BREW" = true ]; then
+  # https://stackoverflow.com/questions/13762280/zsh-compinit-insecure-directories/22753363
+  sudo chmod -R 755 /usr/local/share/zsh
+  sudo chown -R root:staff /usr/local/share/zsh
+
+### zsh
+ln -sf $(grealpath ./shell/profile) ~/.profile
+ln -sf $(grealpath ./shell/zshrc) ~/.zshrc
+ln -sf $(grealpath ./shell/zshrc.local) ~/.zshrc.local
+ln -sf $(grealpath ./shell/ideavimrc) ~/.ideavimrc
+
+ln -sf $(grealpath ./shell/tmux.conf) ~/.tmux.conf
+
+ln -sf $(grealpath ./shell/vimrc) ~/.vimrc
+ln -sf $(grealpath ./shell/gitconfig) ~/.gitconfig
+
+if ! [ -d ~/bin ]; then
+  mkdir ~/bin
+fi
+ln -sf $(grealpath ./git-radar/git-radar) ~/bin/git-radar
+fi
